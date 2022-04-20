b97c3ef681d3bb4f64988efc2624205b09b09852
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-04-20 11:24:31 +0300

    Create response

--- API/API/Models/LoginModel.cs
+++ API/API/Models/LoginModel.cs
@@ -1,6 +1,13 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Username required")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password required")]
+        public string? Password { get; set; }
     }
 }
